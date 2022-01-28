d70092b9848e8b2deb946fc10a90851f5a5c074b
Corrado.Ferra <corradoferra@gmail.com>
2022-01-28 11:59:54 +0100

    v-for nell'header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,26 +2,65 @@
     <div class="header">
         <img src="../assets/dc-logo.png" alt="">
         <ul class="headmen">
-            <li>CHARACTERS</li>
-            <li>COMICS</li>
-            <li>MOVIES</li>
-            <li>TV</li>
-            <li>GAMES</li>
-            <li>COLLECTIBLES</li>
-            <li>VIDEOS</li>
-            <li>FANS</li>
-            <li>NEWS</li>
-            <li>SHOP</li>
+            <li v-for="button in list" :key="button.id">
+                {{button.label}}
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+    return {
+        list:[
+        {
+            label: 'CHARACTERS',
+            id: '0123'
+        },
+        {
+            label: 'COMICS',
+            id: '0124'
+        },
+        {
+            label: 'MOVIES',
+            id: '0125'
+        },
+        {
+            label: 'TV',
+            id: '0126'
+        },
+        {
+            label: 'GAMES',
+            id: '0127'
+        },
+        {
+            label: 'COLLECTIBLES',
+            id: '0128'
+        },
+        {
+            label: 'VIDEOS',
+            id: '0129'
+        },
+        {
+            label: 'FANS',
+            id: '0130'
+        },
+        {
+            label: 'NEWS',
+            id: '0131'
+        },
+        {
+            label: 'SHOP',
+            id: '0132'
+        }
+    ]
+    }
 }
+};
+
 </script>
 
-<style lang="scss">
+<style lang="scss" scope>
 @import '../style/header.scss'
 </style>
