59e31f8a463bf7f4bff48502e5ec27811c4fca69
Corrado.Ferra <corradoferra@gmail.com>
2022-03-04 16:06:03 +0100

    done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,66 +1,65 @@
 <template>
-    <div class="header">
-        <img src="../assets/dc-logo.png" alt="">
-        <ul class="headmen">
-            <li v-for="button in list" :key="button.id">
-                {{button.label}}
-            </li>
-        </ul>
-    </div>
+  <div class="header">
+    <img src="../assets/dc-logo.png" alt="" />
+    <ul class="headmen">
+      <li v-for="button in list" :key="button.id">
+        {{ button.label }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
+  data() {
     return {
-        list:[
+      list: [
         {
-            label: 'CHARACTERS',
-            id: '0123'
+          label: "CHARACTERS",
+          id: "0123",
         },
         {
-            label: 'COMICS',
-            id: '0124'
+          label: "COMICS",
+          id: "0124",
         },
         {
-            label: 'MOVIES',
-            id: '0125'
+          label: "MOVIES",
+          id: "0125",
         },
         {
-            label: 'TV',
-            id: '0126'
+          label: "TV",
+          id: "0126",
         },
         {
-            label: 'GAMES',
-            id: '0127'
+          label: "GAMES",
+          id: "0127",
         },
         {
-            label: 'COLLECTIBLES',
-            id: '0128'
+          label: "COLLECTIBLES",
+          id: "0128",
         },
         {
-            label: 'VIDEOS',
-            id: '0129'
+          label: "VIDEOS",
+          id: "0129",
         },
         {
-            label: 'FANS',
-            id: '0130'
+          label: "FANS",
+          id: "0130",
         },
         {
-            label: 'NEWS',
-            id: '0131'
+          label: "NEWS",
+          id: "0131",
         },
         {
-            label: 'SHOP',
-            id: '0132'
-        }
-    ]
-    }
-}
+          label: "SHOP",
+          id: "0132",
+        },
+      ],
+    };
+  },
 };
-
 </script>
 
 <style lang="scss" scope>
-@import '../style/header.scss'
+@import "../style/header.scss";
 </style>
